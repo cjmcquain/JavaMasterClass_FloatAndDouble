36ac457d1cfdaa6cd8e6b1ee4f065fdde4586625
Charles McQuain <charlesmcquain@gmail.com>
2018-10-09 16:58:03 -0700

    Added int, float, and double values and printed results

--- src/com/cmcquain/Main.java
+++ src/com/cmcquain/Main.java
@@ -3,6 +3,11 @@ package com.cmcquain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myIntValue = 5;
+	    float myFloatValue = 5f /2f;
+	    double myDoubleValue = 5d;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
     }
 }
